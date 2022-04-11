a515c793a342229d271d23faefde558e941bdfd6
Yovko Garkov <iovko1993@gmail.com>
2022-04-11 22:36:25 +0300

    Add CheckPrimeNumber method

--- MultiplicationTable/Program.cs
+++ MultiplicationTable/Program.cs
@@ -6,7 +6,22 @@ namespace MultiplicationTable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = int.Parse(Console.ReadLine());
+
+        }
+
+        // Returns true if the number is prime, and false if its not
+        private bool CheckPrimeNumber(int number)
+        {
+            for (int i = 2; i < number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
