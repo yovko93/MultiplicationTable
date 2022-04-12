eb06deb5922924bebf0268e15dbd7095d0f178e6
Yovko Garkov <iovko1993@gmail.com>
2022-04-12 14:52:29 +0300

    Create and print multiplication table

--- MultiplicationTable/Program.cs
+++ MultiplicationTable/Program.cs
@@ -12,7 +12,35 @@ namespace MultiplicationTable
                 .GetFirstNPrimeNumbers(number)
                 .ToArray();
 
-            Console.WriteLine(string.Join(' ', primeNumbers));
+            int[,] matrix = new int[number + 1, number + 1];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                Console.WriteLine("\n");
+
+                if (row == 0)
+                {
+                    Console.Write("  ");
+
+                    for (int col = 1; col < matrix.GetLength(1); col++)
+                    {
+                        matrix[row, col] = primeNumbers[col - 1];
+                        Console.Write($"{matrix[row, col]}  ");
+                    }
+
+                    continue;
+                }
+
+                matrix[row, 0] = primeNumbers[row - 1];
+                Console.Write($"{matrix[row, 0]} ");
+
+                for (int col = 1; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row - 1, col] = primeNumbers[row - 1] * primeNumbers[col - 1];
+                    Console.Write($"{matrix[row - 1, col]}  ");
+                }
+
+            }
         }
     }
 }
