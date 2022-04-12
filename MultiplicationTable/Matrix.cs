40ea55c5fa1e1a8d07b4468d97a609def18e2a67
Yovko Garkov <iovko1993@gmail.com>
2022-04-12 15:42:52 +0300

    Create new class for matrix methods

---
+++ MultiplicationTable/Matrix.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    public class Matrix
+    {
+        private int[] primeNumbers;
+        private int number;
+
+        public Matrix(int[] primeNumbers)
+        {
+            this.primeNumbers = primeNumbers;
+            this.number = primeNumbers.Length;
+        }
+
+        public void PrintMatrix()
+        {
+            int[,] matrix = new int[number + 1, number + 1];
+
+            ReturnMatrix(matrix);
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                Console.WriteLine("\n");
+
+                if (row == 0)
+                {
+                    Console.Write("  ");
+
+                    for (int col = 1; col < matrix.GetLength(1); col++)
+                    {
+                        Console.Write($"{matrix[row, col]}  ");
+                    }
+
+                    continue;
+                }
+
+                Console.Write($"{matrix[row, 0]} ");
+
+                for (int col = 1; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]}  ");
+                }
+
+            }
+        }
+
+        private int[,] ReturnMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                if (row == 0)
+                {
+                    for (int col = 1; col < matrix.GetLength(1); col++)
+                    {
+                        matrix[row, col] = primeNumbers[col - 1];
+                    }
+
+                    continue;
+                }
+
+                matrix[row, 0] = primeNumbers[row - 1];
+
+                for (int col = 1; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = matrix[row, 0] * primeNumbers[col - 1];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
--- MultiplicationTable/Program.cs
+++ MultiplicationTable/Program.cs
@@ -12,35 +12,8 @@ namespace MultiplicationTable
                 .GetFirstNPrimeNumbers(number)
                 .ToArray();
 
-            int[,] matrix = new int[number + 1, number + 1];
-
-            for (int row = 0; row < matrix.GetLength(0); row++)
-            {
-                Console.WriteLine("\n");
-
-                if (row == 0)
-                {
-                    Console.Write("  ");
-
-                    for (int col = 1; col < matrix.GetLength(1); col++)
-                    {
-                        matrix[row, col] = primeNumbers[col - 1];
-                        Console.Write($"{matrix[row, col]}  ");
-                    }
-
-                    continue;
-                }
-
-                matrix[row, 0] = primeNumbers[row - 1];
-                Console.Write($"{matrix[row, 0]} ");
-
-                for (int col = 1; col < matrix.GetLength(1); col++)
-                {
-                    matrix[row - 1, col] = primeNumbers[row - 1] * primeNumbers[col - 1];
-                    Console.Write($"{matrix[row - 1, col]}  ");
-                }
-
-            }
+            var result = new Matrix(primeNumbers);
+            result.PrintMatrix();
         }
     }
 }
