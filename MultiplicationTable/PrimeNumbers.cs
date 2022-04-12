e738747df7eaed70f3777ad2dbe35c3659d1c05b
Yovko Garkov <iovko1993@gmail.com>
2022-04-12 13:06:39 +0300

    Move prime number methods to new class

---
+++ MultiplicationTable/PrimeNumbers.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MultiplicationTable
+{
+    public class PrimeNumbers
+    {
+        public PrimeNumbers()
+        {
+        }
+
+        public List<int> GetFirstNPrimeNumbers(int n)
+        {
+            var numbers = new List<int>();
+
+            if (n <= 0)
+            {
+                return numbers;
+            }
+
+            if (n >= 1)
+            {
+                numbers.Add(2);
+                n--;
+            }
+            if (n >= 2)
+            {
+                numbers.Add(3);
+                n--;
+            }
+            if (n >= 3)
+            {
+                numbers.Add(5);
+                n--;
+            }
+
+            var result = GetRemainingPrimeNumbers(n, numbers);
+
+            return result;
+        }
+
+        private List<int> GetRemainingPrimeNumbers(int n, List<int> numbers)
+        {
+            if (n == 0)
+            {
+                return numbers;
+            }
+
+            int nextPrimeNumber = FindNextPrimeNumber(numbers.LastOrDefault());
+            numbers.Add(nextPrimeNumber);
+
+            return GetRemainingPrimeNumbers(n - 1, numbers);
+        }
+
+        // Recieves a number
+        // Returns the next prime number after it
+        private int FindNextPrimeNumber(int number)
+        {
+            number++;
+
+            while (!CheckPrimeNumber(number))
+            {
+                number++;
+            }
+
+            return number;
+        }
+
+        // Returns true if the number is prime, and false if its not
+        private bool CheckPrimeNumber(int number)
+        {
+            for (int i = 2; i < number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- MultiplicationTable/Program.cs
+++ MultiplicationTable/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace MultiplicationTable
 {
@@ -10,83 +8,11 @@ namespace MultiplicationTable
         {
             int number = int.Parse(Console.ReadLine());
 
-            IEnumerable<int> result = GetFirstNPrimeNumbers(number);
+            var primeNumbers = new PrimeNumbers()
+                .GetFirstNPrimeNumbers(number)
+                .ToArray();
 
-            foreach (var item in result)
-            {
-                Console.WriteLine(item);
-            }
-        }
-
-        private static IEnumerable<int> GetFirstNPrimeNumbers(int n)
-        {
-            var numbers = new List<int>();
-
-            if (n <= 0)
-            {
-                return numbers;
-            }
-
-            if (n >= 1)
-            {
-                numbers.Add(2);
-                n--;
-            }
-            if (n >= 2)
-            {
-                numbers.Add(3);
-                n--;
-            }
-            if (n >= 3)
-            {
-                numbers.Add(5);
-                n--;
-            }
-
-            var result = GetRemainingPrimeNumbers(n, numbers);
-
-            return result;
-        }
-
-        private static List<int> GetRemainingPrimeNumbers(int n, List<int> numbers)
-        {
-            if (n == 0)
-            {
-                return numbers;
-            }
-
-            int nextPrimeNumber = FindNextPrimeNumber(numbers.LastOrDefault());
-            numbers.Add(nextPrimeNumber);
-
-            return GetRemainingPrimeNumbers(n - 1, numbers);
-        }
-
-        // Recieves a number
-        // Returns the next prime number after it
-        private static int FindNextPrimeNumber(int number)
-        {
-            number++;
-
-            while (!CheckPrimeNumber(number))
-            {
-                number++;
-            }
-
-            return number;
-        }
-
-        // Returns true if the number is prime, and false if its not
-        private static bool CheckPrimeNumber(int number)
-        {
-            for (int i = 2; i < number / 2; i++)
-            {
-                if (number % i == 0)
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            Console.WriteLine(string.Join(' ', primeNumbers));
         }
     }
 }
